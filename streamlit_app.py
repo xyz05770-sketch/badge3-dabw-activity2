3682c47cfebc9f778da462e130af85b56f8467c1
Ramanujan Sadagopan <rsadagopan@phdata.io>
2025-08-26 22:20:06 +0530

    Dropdown view of table

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,16 @@ st.write("Manage your employee records efficiently.")
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("employee_crud.public.employee").select(col('ID'), col('FirstName'), col('LastName'), col('DOB'), col('Gender'), col('Department'), col('Salary'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+my_dataframe = session.table("employee_crud.public.employee").select(
+    col('ID'), col('FirstName'), col('LastName'), col('DOB'),
+    col('Gender'), col('Department'), col('Salary')
+)
+
+if "show_table" not in st.session_state:
+    st.session_state.show_table = False
+
+if st.button("Click to Hide" if st.session_state.show_table else "Click to View"):
+    st.session_state.show_table = not st.session_state.show_table
+
+if st.session_state.show_table:
+    st.dataframe(data=my_dataframe, use_container_width=True)
